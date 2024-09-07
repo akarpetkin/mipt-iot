96af4a971dd4a2581ff0ca7dc189c6eef0ae4031
Карпеткин Александр <alex.karpetkin@gmail.com>
2024-09-07 22:58:29 +0500

    Home tasks

---
+++ hw1/task2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void) {                                  
+    printf("Let's\n\tgo\n\t\tto walk");
+    return 0;         
+}   
+
---
+++ hw1/task3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void print_ascii(void) {
+    printf("   *\n");
+    printf("  ***\n");
+    printf(" *****\n");
+    printf("*******\n");
+    printf(" HHOHH\n");
+    printf(" ZZZZZ\n");
+}
+
+int main(void) {                                  
+    print_ascii();
+    return 0;         
+}   
+
